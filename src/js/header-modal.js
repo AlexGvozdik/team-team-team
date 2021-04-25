89fb4ebdf3599c98e87060ed8dee3cba85e5d2f8
Ivan Faryga <ifaryga@gmail.com>
2021-04-26 02:23:56 +0300

    добавляет модалку в хедер

---
+++ src/js/header-modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[header-data-modal-open]'),
+    closeModalBtn: document.querySelector('[header-data-modal-close]'),
+    modal: document.querySelector('[header-data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
